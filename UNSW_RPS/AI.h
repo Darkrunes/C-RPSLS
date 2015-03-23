b2ffe4d1ecac633358dd5f7b56925af950feccce
Saffat S Akanda <jadenandyubel@hotmail.com>
2015-03-23 17:10:38 +1100

    Created basic layout for future code. Created header files, added guards. Implemented function to display possible hand gestures to user.

---
+++ UNSW_RPS/AI.h
@@ -0,0 +1,8 @@
+#ifndef AI
+#define AI
+
+int generateDecision() {
+
+}
+
+#endif
---
+++ UNSW_RPS/userInput.h
@@ -0,0 +1,11 @@
+#ifndef USERINPUT_H
+#define USERINPUT_H
+
+#include <stdio.h>
+
+
+int getUserInput() {
+
+}
+
+#endif
