640c5c9ef2e77d1f0f3a6a5cfcc6d145910d309a
Saffat S Akanda <jadenandyubel@hotmail.com>
2015-03-30 22:42:52 +1100

    Changed numbering of enumerator and array as bug fix for gameManager.h input. Added comments

--- UNSW_RPS/Choices.h
+++ UNSW_RPS/Choices.h
@@ -4,14 +4,22 @@
 
 enum Choices
 {
-    Rock = 1,
+    Rock,
     Paper,
     Scissors,
+    Spock,
     Lizard,
-    Spock
 };
 
 // Used to display names of selected gestures
-const char *choicesNames[] = { "Rock", "Paper", "Scissors", "Lizard", "Spock" };
+const char *choicesNames[] = { "Rock", "Paper", "Scissors", "Spock", "Lizard" };
+
+/*
+Used to store victories and decisions over 100 games.
+~~~~~~~~~~~~~~~~~~~~~~ TODO: STORE INTO FILE FOR LATER USE ~~~~~~~~~~~~
+*/
+char *playerOneChoices[100];
+char *AIChoices[100];
+int victories[100];
 
 #endif
