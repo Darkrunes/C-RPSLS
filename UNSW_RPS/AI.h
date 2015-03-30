1a8e3a3aea7df4474d8dfff650a6a78c095c984a
Saffat S Akanda <jadenandyubel@hotmail.com>
2015-03-30 22:40:27 +1100

    Added comments

--- UNSW_RPS/AI.h
+++ UNSW_RPS/AI.h
@@ -8,6 +8,10 @@
 
 // Will add analysis to make smarter decision later (probably)
 int generateDecision() {
+    /*
+    A random number generator to pick the opponents moves
+    */
+
     int randomDigit;
 
     randomDigit = (rand() % 5) + 1;
